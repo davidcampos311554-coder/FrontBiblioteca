457d5b48b319658c83854a479beb92b8828a07f9
davidcampos311554@correo.itm.edu.co <gomezdavid872@gmail.com>
2026-05-21 11:01:43 -0500

    Creeate project

---
+++ src/main/java/com/frontend/frontbiblioteca/FrontBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.frontend.frontbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontBibliotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontBibliotecaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frontend/frontbiblioteca/FrontBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontend.frontbiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontBibliotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
