88a5c061551896d41ca8bb3c04d19c017a07cd06
davidcampos311554@correo.itm.edu.co <gomezdavid872@gmail.com>
2026-05-24 01:08:44 -0500

    Inyección de FeignClient

--- src/main/java/com/frontend/frontbiblioteca/FrontBibliotecaApplication.java
+++ src/main/java/com/frontend/frontbiblioteca/FrontBibliotecaApplication.java
@@ -2,8 +2,10 @@ package com.frontend.frontbiblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FrontBibliotecaApplication {
 
     public static void main(String[] args) {
