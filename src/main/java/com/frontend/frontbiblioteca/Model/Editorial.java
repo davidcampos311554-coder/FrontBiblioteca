a893e5a80e07d3e734a1ad12ff8cb38ed6afef09
davidcampos311554@correo.itm.edu.co <gomezdavid872@gmail.com>
2026-05-27 18:38:25 -0500

    Correcta implementación del builder

--- src/main/java/com/frontend/frontbiblioteca/Model/Editorial.java
+++ src/main/java/com/frontend/frontbiblioteca/Model/Editorial.java
@@ -1,10 +1,14 @@
 package com.frontend.frontbiblioteca.Model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Editorial {
     String id;
     String nombre;
--- src/main/java/com/frontend/frontbiblioteca/Model/Escrito.java
+++ src/main/java/com/frontend/frontbiblioteca/Model/Escrito.java
@@ -1,12 +1,16 @@
 package com.frontend.frontbiblioteca.Model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Escrito {
     String id;
     LocalDate fechaEscrito;
