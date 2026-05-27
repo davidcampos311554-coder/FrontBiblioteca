20f1f08ef6f014c897f82b589b6c8d9c73c5db3c
davidcampos311554@correo.itm.edu.co <gomezdavid872@gmail.com>
2026-05-27 18:38:34 -0500

    Correcta implementación del builder

--- src/main/java/com/frontend/frontbiblioteca/Model/Prestamo.java
+++ src/main/java/com/frontend/frontbiblioteca/Model/Prestamo.java
@@ -1,17 +1,21 @@
 package com.frontend.frontbiblioteca.Model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Prestamo {
     String idPrestamo;
     LocalDate fechaPrestamo;
-    LocalTime fechaDevolucion;
+    LocalDate fechaDevolucion;
     Miembro miembro;
     Ejemplar ejemplar;
     Bibliotecario bibliotecario;
